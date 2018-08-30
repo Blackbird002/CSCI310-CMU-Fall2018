abca0763c6a571ca193d33d41b48b40e7260f90d
Riad Shash <riadshash@yahoo.com>
2018-08-30 12:13:48 -0600

    Added a project

---
+++ Test/src/main/java/com/github/blackbird002/test/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.github.blackbird002.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author riads
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) throws Exception {
+        //Creates a new instance of HelloWorld type
+        HelloWorld helloWorld = new HelloWorld();
+
+        //Invoke some method
+        helloWorld.sayMessage();
+
+        int x = 3;
+    }
+
+    private String message = "";
+
+    public void setMessage(String _message) {
+        if (_message != null) {
+            message = _message;
+        } else {
+            throw new UnsupportedOperationException("Message cannot be null!");
+        }
+    }
+    
+    
+
+    private void sayMessage() {
+        System.out.println(getMessage());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
