11b73b08aa28fda2d802d97e4356ac24e4ed1fd9
Riad Shash <riadshash@yahoo.com>
2018-09-10 17:47:29 -0600

    Added tests for Egypt class

--- Assignment1/src/main/java/com/github/blackbird002/assignment1/Egypt.java
+++ Assignment1/src/main/java/com/github/blackbird002/assignment1/Egypt.java
@@ -4,11 +4,51 @@
  * and open the template in the editor.
  */
 package com.github.blackbird002.assignment1;
+import java.util.Scanner;
 
 /**
  *
  * @author riads
  */
 public class Egypt {
+    public static void main(String args[]){
+
+        String inputStr = "";
+        int[] threeInts;
+        boolean result;
+
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            result = false;
+            inputStr = sc.nextLine();
+            threeInts = splitString(inputStr);
+
+            //Exits the program is 0 0 0 is entered
+            if(threeInts[0] == 0 || threeInts[1] == 0 || threeInts[2] == 0)
+                break;
+            
+            //EgyptSolver solver = new EgyptSolver(threeInts);
+
+            //result = solver.isRight();
+
+            if(result == true)
+                System.out.println("right");
+            else
+                System.out.println("wrong");
+        }                        
+    }
     
+    public static int[] splitString(String inputStr){
+        String[] threeNums = {"0","0","0"};
+        int[] threeInts = {0,0,0}; 
+
+        threeNums = inputStr.split(" ");
+
+        for(int i = 0; i <= 2; i++){
+            //Type cast the string to an int
+            threeInts[i] = Integer.valueOf(threeNums[i]);
+        }
+        return threeInts;     
+    }
 }
+
---
+++ Assignment1/src/test/java/com/github/blackbird002/assignment1/EgyptTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.blackbird002.assignment1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author riads
+ */
+public class EgyptTest {
+    
+    public EgyptTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Egypt.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Egypt.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of splitString method, of class Egypt.
+     */
+    @Test
+    public void testSplitString() {
+        System.out.println("splitString");
+        String inputStr = "";
+        int[] expResult = null;
+        int[] result = Egypt.splitString(inputStr);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
