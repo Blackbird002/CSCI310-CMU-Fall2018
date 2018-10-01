55aa7617d39d4d852ebacb1f260d1894092be32d
Riad Shash <riadshash@yahoo.com>
2018-10-01 10:52:41 -0600

    Added Customer and WaitSimulator classes

---
+++ CustomerWaitSimulator/src/main/java/com/github/blackbird002/customerwaitsimulator/WaitSimulator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.blackbird002.customerwaitsimulator;
+
+/**
+ *
+ * @author riads
+ * Assignment #2 for CSCI 310 - Java
+ * Program simulates a line at a grocery store with 3 cash registers implemented
+ * as a queue data structure. 
+ * This program will use the Java collections library 
+ */
+public class WaitSimulator {
+    
+    public static void main(String args[]){
+        
+    }  
+}
