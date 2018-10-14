2b1999ab86d06847ddb498269d37b1eab3bf36ab
Riad Shash <riadshash@yahoo.com>
2018-10-14 11:42:24 -0600

    Added tests for the ProcessObject class

--- Assignment3/src/test/java/com/github/blackbird002/assignment3/ProcessObjectTest.java
+++ Assignment3/src/test/java/com/github/blackbird002/assignment3/ProcessObjectTest.java
@@ -5,6 +5,7 @@
  */
 package com.github.blackbird002.assignment3;
 
+import java.util.LinkedList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,12 +44,30 @@ public class ProcessObjectTest {
     @Test
     public void testGetId() {
         System.out.println("getId");
-        ProcessObject instance = new ProcessObject();
-        int expResult = 0;
+        ProcessObject instance = new ProcessObject(25);
+        int expResult = 25;
         int result = instance.getId();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of printVisitedList method, of class ProcessObject.
+     */
+    @Test
+    public void testPrintVisitedList() {
+        Demonstration demo = new Demonstration();
+        demo.doDemonstration();
+        
+        int numPresent = 0;
+        int expNumPresent = 4;
+        for(String line : demo.objectsToProcess[0].visitedlist){
+               //Tests if each thread added a string to the visitedlist
+               assertTrue(line.startsWith("Thread Child"));  
+               numPresent++;
+        }
+        //Tests if 4 threads visited the object
+        assertEquals(expNumPresent,numPresent);
+           
     }
     
 }
